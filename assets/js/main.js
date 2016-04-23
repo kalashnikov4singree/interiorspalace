8d68cc42717353f6aac03c26634eef53a2c89c05
Vitaliy Kalashnikov <v1pboy0730@gmail.com>
2016-04-23 19:45:52 +0300

    Main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,19 +14,6 @@ function arrowDown(){
         return false;
     });
 }
-// carousel mode: random
-function getRandomOptions(){
-    // this could obviously be a lot more random
-    var doIt = Math.floor(Math.random() * 1001) % 2 == 0;
-    // set random options supported by the goto method
-    // http://www.drewgreenwell.com/projects/metrojs#liveTileMethods
-    return {
-        index: "next",
-        delay: 3000,
-        animationDirection: doIt ? 'forward' : 'backward',
-        direction: doIt ? 'vertical' : 'horizontal'
-    };
-}
 // setup the tile and then call goto on it the first time
 $(document).ready(function(){
     $('.bg-slider').owlCarousel({
@@ -39,11 +26,7 @@ $(document).ready(function(){
         mouseDrag: false,
         loop:true
     });
-    $(".live-tile").liveTile({
-        animationComplete: function(tileData){
-            $(this).liveTile("goto", getRandomOptions());
-        }
-    }).liveTile("goto", getRandomOptions());
+    $(".live-tile").liveTile();
     heightMainSlider();
     arrowDown();
 });
